0403e3515389e6c0f011a2ec4f98b48ed9872b14
HoangPham <hoangpham28121997@gmail.com>
2019-10-28 09:21:02 +0700

    Chapter 9: Advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,34 @@
 class SessionsController < ApplicationController
+  before_action :findusers, only: [:create]
   def new; end
 
   def create
-    user = User.find_by email: params[:session][:email].downcase
-    if user&.authenticate params[:session][:password]
-      log_in user
-      redirect_to user
+    if @user.authenticate params[:session][:password]
+      log_in @user
+      if params[:session][:remember_me] == Settings.session_numbers
+        remember @user
+      else
+        forget @user
+      end
+      redirect_to @user
     else
-      flash[:danger] = t "invalid_combination"
+      flash.now[:danger] = t "invalid_info"
       render :new
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def findusers
+    @user = User.find_by email: params[:session][:email].downcase
+    return if @user
+
+    flash.now[:danger] = t "invalid_info"
+    render :new
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = Settings.email_regex
+  attr_accessor :remember_token
 
   before_save{email.downcase!}
 
@@ -14,9 +15,31 @@ class User < ApplicationRecord
 
   class << self
     def digest string
-      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-        BCrypt::Engine.cost
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
       BCrypt::Password.create string, cost: cost
     end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update remember_digest: nil
   end
 end
